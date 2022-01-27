d86787809c099638c74c2fc29ece4d3de5240dc2
Erfanprg1701 <erfan.prg1701@gmail.com>
2022-01-27 17:37:29 +0330

    Practice

---
+++ map/map.js
@@ -0,0 +1,9 @@
+// Map 
+
+const myMap = new Map();
+myMap.set("name", "erfan");
+myMap.set(["name","age"], "erfan-16");
+
+console.log(myMap)
+// console.log(myMap.get("name"))
+// console.log(myMap.size)
---
+++ set/set.js
@@ -0,0 +1,14 @@
+// Set
+
+const mySet = new Set();
+mySet.add("erfan")
+mySet.add("milad")
+mySet.add("mamad")
+mySet.add("ali")
+console.log(mySet)
+
+mySet.forEach(item => console.log(item))
+
+// const array = [1,1,2,3,3,4,4,4,4];
+// const newSet = new Set(array);
+// newSet.forEach(item => console.log(item))
